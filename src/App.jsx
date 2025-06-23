422e575978c04986354efd4e2174c5935435f714
GitHubKing23 <152663285+GitHubKing23@users.noreply.github.com>
2025-06-23 01:55:37 -0400

    Scaffold admin features and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import ShortenerForm from './ShortenerForm.jsx'
+import AdminLogin from './pages/AdminLogin.jsx'
+import AdminDashboard from './pages/AdminDashboard.jsx'
+import NotFound from './pages/NotFound.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 import './App.css'
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <ShortenerForm />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ShortenerForm />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer position="bottom-right" />
+    </BrowserRouter>
   )
 }
 
