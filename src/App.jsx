b241fab2b3f2ed5597e048fbb4204db74de57db5
Jeff <wise11jeff@gmail.com>
2025-06-23 08:26:44 -0400

    Manually resolved App.jsx merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
 import ShortenerForm from './ShortenerForm.jsx'
 import AdminLogin from './pages/AdminLogin.jsx'
 import AdminDashboard from './pages/AdminDashboard.jsx'
 import NotFound from './pages/NotFound.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
-
 import './App.css'
 
 function App() {
